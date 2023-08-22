6d02eab9a50c882b95729e95cd9603ebd828f737
Tusharhub <60460907+Tusharhub@users.noreply.github.com>
2023-08-22 15:26:36 +0530

    First

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// const serverless = require("serverless-http");
 const crypto = require("crypto");
 const axios = require("axios");
 const path = require("path");
+
+// const router = express.Router();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "/public"));
 
 // PhonePe API credentials
 const MERCHANT_KEY = "82aeb5a9-d02e-4398-8dc9-ede90e40eb2d";
@@ -20,16 +23,16 @@ app.get("/", (req, res) => {
 
 // Handle form submission
 app.post("/initiate-payment", async (req, res) => {
-  const amount = req.body.amountEnterByUsers;
+  const amountInPaise = req.body.amountEnterByUsers * 100; // Amount in paise
 
   const data = {
     merchantId: "M1KDULVI2IHU",
     merchantTransactionId: "MT" + Date.now(),
     merchantUserId: "MIT1",
-    amount: amount * 100,
+    amount: amountInPaise, // Amount in paise
     redirectUrl: "http://localhost:3000/payment-status",
     redirectMode: "POST",
-    callbackUrl: "http://localhost:3000/payment-status",
+    callbackUrl: "http://localhost:3000/payment-status", // Server-to-server callback URL
     mobileNumber: "9718903887",
     paymentInstrument: {
       type: "PAY_PAGE",
@@ -43,7 +46,7 @@ app.post("/initiate-payment", async (req, res) => {
 
   try {
     const response = await axios.post(
-      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay",
+      "https://api.phonepe.com/apis/hermes",
       { request: payloadMain },
       {
         headers: {
@@ -63,8 +66,6 @@ app.post("/initiate-payment", async (req, res) => {
 });
 
 // Payment status route
-// Payment status route
-// Payment status route
 app.get("/payment-status", (req, res) => {
   // Retrieve query parameters from the URL
   const code = req.query.code;
@@ -72,10 +73,12 @@ app.get("/payment-status", (req, res) => {
   const providerReferenceId = req.query.providerReferenceId;
 
   // Redirect to payment.html with query parameters
-  res.sendFile(__dirname + "/public/payment.html");
-  res.sendFile(path.join(__dirname, "public", "payment.html"));
+  res.redirect(
+    `/public/payment.html?code=${code}&transactionId=${transactionId}&providerReferenceId=${providerReferenceId}`
+  );
 });
 
+// Start the server
 // Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
